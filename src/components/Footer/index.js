d2122c4c78263acaf932fd4a05fa6ce1b0b844ca
haeyong9701 <haeyong9701@gmail.com>
2025-03-28 12:08:24 +0900

    chore: footer 수정

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,9 +8,9 @@ const Footer = () => {
         <div className="info">
           <div>사업명: 더샵 신문 그리니티</div>
           <div>사업지 주소: 경상남도 김해시 신문1지구 A7-1블록</div>
-          {/* <div>사이트 관리자: 조훈례</div> */}
+          <div>사이트 관리자: 조훈례</div>
           <div>이메일: chr196667@naver.com</div>
-          {/* <div>전화번호 : 010-9316-8671</div> */}
+          <div>전화번호 : 010-9316-8671</div>
           <div></div>
         </div>
         <div className="sub-title">
