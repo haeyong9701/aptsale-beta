c901a3082ebb13f786a7f8939a8d0542738de7c3
haeyong9701 <haeyong9701@gmail.com>
2025-03-28 12:15:06 +0900

    chore: footer 수정

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,7 +10,7 @@ const Footer = () => {
           <div>사업지 주소: 경상남도 김해시 신문1지구 A7-1블록</div>
           <div>사이트 관리자: 조훈례</div>
           <div>이메일: chr196667@naver.com</div>
-          <div>전화번호 : 010-9316-8671</div>
+          <div>전화번호 : 010-2537-9316</div>
           <div></div>
         </div>
         <div className="sub-title">
